e4cf73b03484b7bf76e24a718ff344ed428f9683
oO-Blink-Oo <alvarezian23@gmail.com>
2020-03-24 10:05:24 -0700

    prototype done

---
+++ TextAdventure/Assets/AdventureGame.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour
+{
+    [SerializeField] Text textComponent; //serializeField makes this editable in the inspector
+    [SerializeField] State StartingState;
+
+
+    State state;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = StartingState;
+        textComponent.text = state.GetStateStory();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ManageState();
+    }
+
+    private void ManageState()
+    {
+        var nextStates = state.GetNextStates();
+
+        for (int i = 0; i < nextStates.Length; i++) {
+            if (Input.GetKeyDown(KeyCode.Alpha1 + i))
+            {
+                state = nextStates[0];
+                textComponent.text = state.GetStateStory();
+            }
+        }
+    }
+}
